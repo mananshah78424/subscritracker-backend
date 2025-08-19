3b7831c67c562d8272843a60ed56ffdef3782872
Manan <157731770+mananshah78424@users.noreply.github.com>
2025-08-19 00:51:59 -0700

    Revert "chore/create migrations for due date logic (#11)" (#14)
    This reverts commit f7b0cbd187aa354315acd00f9702c39b05dcc169.

--- db/migrations/20250818223047_add_due_type.up.sql
+++
@@ -1,6 +0,0 @@
--- Add due_type column to support different subscription frequencies
-ALTER TABLE subscription_details 
-ADD COLUMN due_type VARCHAR(255) NOT NULL DEFAULT 'monthly' CHECK (due_type IN ('monthly', 'yearly', 'custom', 'weekly', 'daily'));
-
-
-
